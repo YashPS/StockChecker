db93818898d50738cea93bdc6bbd54dea5dbaf0a
yashps <yashpsingh1206@gmail.com>
2022-07-27 01:03:39 +0530

    it works with config!

--- checker.py
+++ checker.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import time
 from selenium import webdriver
+import yaml
+
+with open('./config/config.yaml') as f:
+    conf = yaml.safe_load(f)
 
 
 def send_msg(text):
-    token = "5451194926:AAEha4fw17IxW3B-10W8BryW9vTod5DQoow"
-    chat_id = "1502268415"
+    token = conf['token']
+    chat_id = conf['chat_id']
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
     results = requests.get(url_req)
     print(results.json())
@@ -25,18 +29,18 @@ def get_page_html(url):
 
 def check_item_in_stock(page_html):
     soup = BeautifulSoup(page_html, 'html.parser')
-    out_of_stock_divs = '"stockstatus":"soldout"' in str(soup.find_all(class_="ps-btn ps-blue"))
+    out_of_stock_divs = conf['check_phrase'] in str(soup.find_all(class_=conf['class_name']))
     print(out_of_stock_divs)
     return out_of_stock_divs
 
 
 def check_inventory():
-    url1 = "https://www.dell.com/en-in/shop/alienware-34-curved-qd-oled-gaming-monitor-aw3423dw/apd/210-bebh/monitors-monitor-accessories"
-    url2 = "https://www.dell.com/en-in/shop/dell-24-video-conferencing-monitor-s2422hz/apd/210-bcbp/monitors-monitor-accessories"
+    url1 = conf['request_url']
+    url2 = conf['test_url']
     page_html = get_page_html(url1)
     if not check_item_in_stock(page_html):
         print("In stock")
-        send_msg("Dell Alienware QD-OLED is in stock!")
+        send_msg(f"{conf['item_name']} is in stock!")
     else:
         print("Out of stock")
 
